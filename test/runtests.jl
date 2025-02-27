6f250067f5a631a2c2893cc0766fa72a83d65e4d
Orso Meneghini <orso82@gmail.com>
2025-02-27 13:38:46 -0800

    formatting tests source

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,17 +17,17 @@ pend = 0.1
 @testset "test_efit01" begin
     model_name = :d3d_efit01
     green = EGGO.get_greens_function_tables(model_name)
-    basis_functions = EGGO.get_basis_functions(model_name,green)
-    basis_functions_1d,bf1d_itp = EGGO.get_basis_functions_1d(model_name)
+    basis_functions = EGGO.get_basis_functions(model_name, green)
+    basis_functions_1d, bf1d_itp = EGGO.get_basis_functions_1d(model_name)
 
     wall = EGGO.get_wall(model_name)
     NNmodel = EGGO.get_model(model_name)
     model = NNmodel[:model]
 
-    Jt,psirz, Ip,fcurrt = EGGO.predict_model(Rb_target,Zb_target,pp_target,ffp_target,ecurrt_target,NNmodel, green, basis_functions,basis_functions_1d)
+    Jt, psirz, Ip, fcurrt = EGGO.predict_model(Rb_target, Zb_target, pp_target, ffp_target, ecurrt_target, NNmodel, green, basis_functions, basis_functions_1d)
 
     dd = IMAS.dd()
-    dd = EGGO.get_surfaces(dd,Matrix(transpose(psirz)),Ip,fcurrt,green,wall,Rb_target,Zb_target,pp_target,ffp_target,ecurrt_target,Btcenter,Rcenter,pend)
+    dd = EGGO.get_surfaces(dd, Matrix(transpose(psirz)), Ip, fcurrt, green, wall, Rb_target, Zb_target, pp_target, ffp_target, ecurrt_target, Btcenter, Rcenter, pend)
 
     @test Jt !== nothing
     @test psirz !== nothing
